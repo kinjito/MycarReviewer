077afcecb34890c432d957d6330e59b15954e7b2
kinjito <destiniyfreedom@gmail.com>
2021-11-27 09:49:17 +0000

    メーカー設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Maker.find_or_create_by(maker_name: "日産")
+Maker.find_or_create_by(maker_name: "トヨタ")
+Maker.find_or_create_by(maker_name: "スバル")
+Maker.find_or_create_by(maker_name: "ホンダ")
+Maker.find_or_create_by(maker_name: "マツダ")
+Maker.find_or_create_by(maker_name: "三菱")
+Maker.find_or_create_by(maker_name: "ダイハツ")
+Maker.find_or_create_by(maker_name: "スズキ")
+Maker.find_or_create_by(maker_name: "その他")
