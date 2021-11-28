1e994d58e5553bef76bccd493bd621f4a675c9ae
kinjito <destiniyfreedom@gmail.com>
2021-11-28 03:55:07 +0000

    魅力についてのページ編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# 条件を指定して初めの1件を取得し1件もなければ作成(create)
 Maker.find_or_create_by(maker_name: "日産")
 Maker.find_or_create_by(maker_name: "トヨタ")
 Maker.find_or_create_by(maker_name: "スバル")
@@ -15,3 +17,8 @@ Maker.find_or_create_by(maker_name: "三菱")
 Maker.find_or_create_by(maker_name: "ダイハツ")
 Maker.find_or_create_by(maker_name: "スズキ")
 Maker.find_or_create_by(maker_name: "その他")
+
+Theme.find_or_create_by(theme_text: "車が好きになったきっかけ")
+Theme.find_or_create_by(theme_text: "所有して良かったこと")
+Theme.find_or_create_by(theme_text: "ドライブに合うお気に入りの音楽")
+
